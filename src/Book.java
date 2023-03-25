0c0fe306ca7522f8bdd596fa1d3220a8801ccef6
Vladimir <vovaykunin@gmail.com>
2023-03-25 19:38:38 +0300

    ДЗ 13 Дополнил 1

--- src/Book.java
+++ src/Book.java
@@ -1,18 +1,27 @@
 public class Book {
-    private Author author;
-    private String name;
-    int year;
 
-    public Book(String name, int year) {
+    private final String name;
+    private final Author author;
+    private int year;
+
+    public Book(String name, int year, Author author) {
         this.name = name;
         this.year = year;
         this.author = author;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,15 @@ public class Main {
         Author author1 = new Author("Талеб Нассим Николас");
 
 
-        Book book = new Book("От хорошего к великому", 2022);
-        System.out.println("Автор- " + author.getName());
-        System.out.println("Название- " + book.getName());
-        System.out.println("Год- " + book.year);
+        Book book = new Book("От хорошего к великому",2022,author);
+        System.out.println("Автор - " + author.getName());
+        System.out.println("Название - " + book.getName());
+        System.out.println("Год - " + book.getYear());
 
-        Book book1 = new Book("Рискуя собственной шкурой", 2022);
+        Book book1 = new Book("Рискуя собственной шкурой", 2022,author);
         System.out.println("Автор - " + author1.getName());
         System.out.println("Название - " + book1.getName());
-        System.out.println("Год - " + book1.year);
+        System.out.println("Год - " + book1.getYear());
 
     }
 }
